64fafcab88f2165f4af9d66bd5bed2918366e80c
alexivany <alexander.ivany@gmail.com>
2024-03-20 20:04:48 -0230

    Added tutorial section to About Modal

--- Scripts/songapp-sidebar.js
+++ Scripts/songapp-sidebar.js
@@ -159,12 +159,34 @@ export default class SongAppSidebar {
       "SongNote is a note taking app made for audio engineers, mixing assistants and recording artists everywhere. Write notes with songs broken down into multiple versions, keeping track of all your mixing notes throughout a song's life. Easily transfer between devices with the download / upload feature, and take advantage of built-in guitar & bass tablature, for memorizing any sections that might need to be recorded or overdubbed.";
     const infoModalContact = document.createElement("p");
     infoModalContact.innerText = `Contact with any concerns or comments:
-    alexander.ivany@gmail.com`;
+    ivany.world@gmail.com`;
     const infoModalOkBtn = document.createElement("button");
     infoModalOkBtn.innerText = "OK";
+    const infoModalArrow = document.createElement("img");
+    infoModalArrow.classList.add("info-arrow-right");
+    infoModalArrow.src = "./SVG/arrow-right.svg";
+    const infoModalCircle1 = document.createElement("div");
+    infoModalCircle1.classList.add("info-circle");
+    infoModalCircle1.classList.add("info-circle-filled");
+    const infoModalCircle2 = document.createElement("div");
+    infoModalCircle2.classList.add("info-circle");
+    const infoModalCircle3 = document.createElement("div");
+    infoModalCircle3.classList.add("info-circle");
+    const infoModalCircleContainer = document.createElement("div");
+    const infoModalArrowLeft = document.createElement("img");
+    infoModalArrowLeft.classList.add("info-arrow-left");
+    infoModalArrowLeft.src = "./SVG/arrow-left.svg";
+    infoModalArrowLeft.style.display = "none";
+    infoModalCircleContainer.classList.add("info-circle-container");
+    infoModalCircleContainer.appendChild(infoModalCircle1);
+    infoModalCircleContainer.appendChild(infoModalCircle2);
+    infoModalCircleContainer.appendChild(infoModalCircle3);
     infoModal.appendChild(infoModalText);
     infoModal.appendChild(infoModalContact);
     infoModal.appendChild(infoModalOkBtn);
+    infoModal.appendChild(infoModalArrow);
+    infoModal.appendChild(infoModalArrowLeft);
+    infoModal.appendChild(infoModalCircleContainer);
     document.querySelector("body").appendChild(infoModal);
 
     document.getElementById("song-app").style.opacity = "0.25";
@@ -177,6 +199,84 @@ export default class SongAppSidebar {
       infoModal.remove();
       document.removeEventListener("click", removeInfoModal);
       document.getElementById("song-app").style.opacity = "1";
+    });
+
+    infoModalArrow.addEventListener("click", () => {
+      if (infoModalCircle1.classList.contains("info-circle-filled")) {
+        infoModalArrowLeft.style.display = "inline";
+        const versionTutImg = document.createElement("img");
+        if (
+          window
+            .getComputedStyle(document.body)
+            .getPropertyValue("background-color") === "rgba(0, 0, 0, 0)"
+        ) {
+          versionTutImg.src = "./Github-src/VersionTut.png";
+        } else {
+          versionTutImg.src = "./Github-src/VersionTutDark.png";
+        }
+        versionTutImg.classList.add("tutorial-image");
+        infoModal.prepend(versionTutImg);
+        infoModalText.innerText =
+          "Add different versions of your song by clicking the Add Version Button, then easily switch between them using the version tabs.";
+        infoModalContact.innerText = "";
+        infoModalCircle1.classList.remove("info-circle-filled");
+        infoModalCircle2.classList.add("info-circle-filled");
+      } else if (infoModalCircle2.classList.contains("info-circle-filled")) {
+        infoModalArrow.style.display = "none";
+        infoModalArrowLeft.style.display = "inline";
+        document.querySelector(".tutorial-image").remove();
+        const versionTutImg = document.createElement("img");
+        if (
+          window
+            .getComputedStyle(document.body)
+            .getPropertyValue("background-color") === "rgba(0, 0, 0, 0)"
+        ) {
+          versionTutImg.src = "./Github-src/InstrumentTut.png";
+        } else {
+          versionTutImg.src = "./Github-src/InstrumentTutDark.png";
+        }
+        versionTutImg.classList.add("tutorial-image");
+        infoModalText.innerText =
+          "Add pre-built instruments using the drop-down list, or create custom instruments by typing them in.";
+        infoModal.prepend(versionTutImg);
+        infoModalCircle2.classList.remove("info-circle-filled");
+        infoModalCircle3.classList.add("info-circle-filled");
+      }
+    });
+
+    infoModalArrowLeft.addEventListener("click", () => {
+      if (infoModalCircle3.classList.contains("info-circle-filled")) {
+        infoModalArrow.style.display = "inline";
+        infoModalArrowLeft.style.display = "inline";
+        document.querySelector(".tutorial-image").remove();
+        const versionTutImg = document.createElement("img");
+        if (
+          window
+            .getComputedStyle(document.body)
+            .getPropertyValue("background-color") === "rgba(0, 0, 0, 0)"
+        ) {
+          versionTutImg.src = "./Github-src/VersionTut.png";
+        } else {
+          versionTutImg.src = "./Github-src/VersionTutDark.png";
+        }
+        versionTutImg.classList.add("tutorial-image");
+        infoModal.prepend(versionTutImg);
+        infoModalText.innerText =
+          "Add different versions of your song by clicking the Add Version Button, then easily switch between them using the version tabs.";
+        infoModalContact.innerText = "";
+        infoModalCircle3.classList.remove("info-circle-filled");
+        infoModalCircle2.classList.add("info-circle-filled");
+      } else if (infoModalCircle2.classList.contains("info-circle-filled")) {
+        document.querySelector(".tutorial-image").remove();
+        infoModalArrow.style.display = "inline";
+        infoModalArrowLeft.style.display = "none";
+        infoModalText.innerText =
+          "SongNote is a note taking app made for audio engineers, mixing assistants and recording artists everywhere. Write notes with songs broken down into multiple versions, keeping track of all your mixing notes throughout a song's life. Easily transfer between devices with the download / upload feature, and take advantage of built-in guitar & bass tablature, for memorizing any sections that might need to be recorded or overdubbed.";
+        infoModalContact.innerText = `Contact with any concerns or comments:
+        ivany.world@gmail.com`;
+        infoModalCircle2.classList.remove("info-circle-filled");
+        infoModalCircle1.classList.add("info-circle-filled");
+      }
     });
   }
 }
